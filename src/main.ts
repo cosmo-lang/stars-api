58cb81b833f205d5257bcfa86a27c1a27b35ab33
R-unic <roleyspersonal@gmail.com>
2023-06-08 03:12:20 -0700

    feat: add GET endpoints

--- src/main.ts
+++ src/main.ts
@@ -1,19 +1,49 @@
-import express, { Request, Response } from "express";
+import { PrismaClient } from "@prisma/client";
+import express from "express";
 
+const prisma = new PrismaClient;
 const app = express();
 const port = 3030;
 
+app.use(express.json());
 const apiRouter = express.Router();
 
-apiRouter.get("/packages/:author", (req, res) => {
+apiRouter.get("/packages/:author", async (req, res) => {
+  try {
+    const packages = await prisma.package.findMany({
+      where: { author: req.params.author }
+    })
+    res.json(packages);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: "Failed to fetch package." });
+  }
+});
+
+apiRouter.post("/packages", (req, res) => {
+  const authorName = req.body.name
   res.send("Todo");
 });
 
-apiRouter.get("/packages/:author/:name", (req, res) => {
-  res.send("Todo");
+apiRouter.get("/packages/:author/:name", async (req, res) => {
+  try {
+    const packageData = await prisma.package.findFirst({
+      where: { author: req.params.author }
+    })
+
+    if (packageData == null)
+      res.status(500).json({ error: "Package does not exist." });
+    else
+      res.json(packageData);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: "Failed to fetch package." });
+  }
 });
 
 apiRouter.post("/packages/:author", (req, res) => {
+  const author = req.params.author
+  const packageName = req.body.name
   res.send("Todo");
 });
 
